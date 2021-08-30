fa53b3faf01bf034ae7565798d1e81772350fe14
百小僧 <monksoul@outlook.com>
2021-08-30 12:45:00 +0800

    💋 添加字符串拓展注释

--- framework/Furion/ObjectExtensions/StringExtensions.cs
+++ framework/Furion/ObjectExtensions/StringExtensions.cs
@@ -5,6 +5,13 @@
 /// </summary>
 internal static class StringExtensions
 {
+    /// <summary>
+    /// 移除字符串指定后缀
+    /// </summary>
+    /// <param name="str">字符串</param>
+    /// <param name="suffix">后缀字符串</param>
+    /// <param name="comparisonType">字符串比较方式</param>
+    /// <returns></returns>
     internal static string SubSuffix(this string str, string suffix, StringComparison comparisonType = StringComparison.Ordinal)
     {
         if (!str.EndsWith(suffix, comparisonType)) return str;

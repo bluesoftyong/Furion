a1cd26f6b6c407cdc95fee5066776cc29a967fdd
百小僧 <monksoul@outlook.com>
2021-09-01 17:01:15 +0800

    🎭 添加框架源代码文件头开源协议说明

--- framework/Furion/Options/Extensions/OptionsServiceCollectionExtensions.cs
+++ framework/Furion/Options/Extensions/OptionsServiceCollectionExtensions.cs
@@ -1,4 +1,12 @@
-﻿using Furion.ObjectExtensions;
+﻿// Copyright (c) 2020-2021 百小僧, Baiqian Co.,Ltd.
+// Furion is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             https://gitee.com/dotnetchina/Furion/blob/master/LICENSE
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+using Furion.ObjectExtensions;
 using Furion.Options;
 using Furion.Options.Extensions;
 using Microsoft.Extensions.Configuration;

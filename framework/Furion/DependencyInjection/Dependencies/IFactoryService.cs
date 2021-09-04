31e5db0547de1220d61da994de7dbecf78e39409
百小僧 <monksoul@outlook.com>
2021-09-04 20:52:19 +0800

    ✔️ 添加服务依赖工厂注册方式

---
+++ framework/Furion/DependencyInjection/Dependencies/IFactoryService.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2020-2021 百小僧, Baiqian Co.,Ltd.
+// Furion is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             https://gitee.com/dotnetchina/Furion/blob/master/LICENSE
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+namespace Furion.DependencyInjection;
+
+/// <summary>
+/// 基于工厂的服务依赖接口
+/// </summary>
+public interface IFactoryService
+{
+    /// <summary>
+    /// 服务工厂
+    /// </summary>
+    /// <param name="serviceProvider"></param>
+    /// <returns></returns>
+    object ServiceFactory(IServiceProvider serviceProvider);
+}
+
+/// <summary>
+/// 基于工厂的服务依赖接口
+/// </summary>
+public interface IFactoryService<TServiceType>
+    where TServiceType : class
+{
+    /// <summary>
+    /// 服务工厂
+    /// </summary>
+    /// <param name="serviceProvider"></param>
+    /// <returns></returns>
+    TServiceType ServiceFactory(IServiceProvider serviceProvider);
+}

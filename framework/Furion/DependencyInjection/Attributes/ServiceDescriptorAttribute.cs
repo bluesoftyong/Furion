564dbae716c5e17633a80537bba8bde63442c463
百小僧 <monksoul@outlook.com>
2021-09-05 21:09:27 +0800

    ✔️ 添加服务描述器特性

---
+++ framework/Furion/DependencyInjection/Attributes/ServiceDescriptorAttribute.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2020-2021 百小僧, Baiqian Co.,Ltd.
+// Furion is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             https://gitee.com/dotnetchina/Furion/blob/master/LICENSE
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+namespace System;
+
+/// <summary>
+/// 服务描述器特性
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public sealed class ServiceDescriptorAttribute : Attribute
+{
+    /// <summary>
+    /// 注册类型
+    /// </summary>
+    public RegisterTypes RegisterType { get; set; } = RegisterTypes.Add;
+}
---
+++ framework/Furion/DependencyInjection/Enums/RegisterTypes.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2020-2021 百小僧, Baiqian Co.,Ltd.
+// Furion is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             https://gitee.com/dotnetchina/Furion/blob/master/LICENSE
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+namespace System;
+
+/// <summary>
+/// 注册方式
+/// </summary>
+public enum RegisterTypes
+{
+    /// <summary>
+    /// 添加注册，无论是否已注册
+    /// </summary>
+    Add,
+
+    /// <summary>
+    /// 如果已注册，则跳过
+    /// </summary>
+    TryAdd
+}

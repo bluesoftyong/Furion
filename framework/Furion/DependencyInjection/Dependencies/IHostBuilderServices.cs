92f2ed8086fdb058317ea355a6b01eddc0f61fd0
百小僧 <monksoul@outlook.com>
2021-09-07 14:40:19 +0800

    😪 整理依赖注入接口目录

---
+++ framework/Furion/DependencyInjection/Dependencies/IHostBuilderServices.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2020-2021 百小僧, Baiqian Co.,Ltd.
+// Furion is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             https://gitee.com/dotnetchina/Furion/blob/master/LICENSE
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Furion.DependencyInjection;
+
+/// <summary>
+/// 配置主机服务
+/// </summary>
+public interface IHostBuilderServices
+{
+    /// <summary>
+    /// 配置服务
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="context"></param>
+    void Configure(IServiceCollection services, HostBuilderContext context);
+}

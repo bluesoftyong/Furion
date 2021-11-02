17036d569b21c07b55df48f74f3e7f3cdca1abad
百小僧 <monksoul@outlook.com>
2021-11-02 11:13:33 +0800

    🎭 新增 TimeCrontab 示例

--- samples/Furion.TimeCrontabSamples/Controllers/TimeCrontabController.cs
+++ samples/Furion.TimeCrontabSamples/Controllers/TimeCrontabController.cs
@@ -13,4 +13,11 @@ public class TimeCrontabController : ControllerBase
         var crontab = Crontab.Parse(cron, format);
         return crontab.GetNextOccurrence(DateTime.Now);
     }
+
+    [HttpPost]
+    public string Parse([FromBody] string cron, CronStringFormat format = CronStringFormat.Default)
+    {
+        var crontab = Crontab.Parse(cron, format);
+        return crontab.ToString();
+    }
 }
